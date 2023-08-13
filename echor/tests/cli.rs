4abaa02d2f614ae030423dd332eee0f2a4f2a1ef
anandg112 <anandg112@gmail.com>
2023-08-13 15:42:34 -0400

    feat: add more tests

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,16 +1,46 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
+
+type TestResult = Result <(), Box<dyn std::error::Error>>; //Ok part of TestResult will only ever hold the unit type and Err part can hold anything that implemenets the std::error:Error trait
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?; //Use ? instead of Result::unwrap to unpack an Ok value or propagate an Err
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    Ok(()) //omit the semicolon to implicitly return that reult
+}
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    
+    Ok(())
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_new_line() -> TestResult {
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_new_line() -> TestResult {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
